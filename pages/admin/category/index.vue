76006ef0985ad76f394bf59dfdccf811c5531cb3
ASIA-890-Dao-Ngoc_Tuyen <tuyendn@ans-asia.com>
2024-10-03 17:24:56 +0700

    fix

--- pages/admin/category/index.vue
+++ pages/admin/category/index.vue
@@ -4,9 +4,9 @@ import Layout from "@/components/layout/admin/layout.vue";
 import TextInput from "@/components/form/TextInput.vue";
 import VButton from "@/components/form/Button.vue";
 import Autocomplete from "@/components/ui/Autocomplete/Autocomplete.vue";
-import Pagination from "@/components/ui/Pagination/Pagination.vue";
 import VCard from "@/components/form/Card.vue";
-import type Category from "~/layers/modals/Category.vue";
+import VTable from "@/components/form/Table.vue";
+
 const data = ref({
   id: "",
   slug: "",
@@ -24,6 +24,70 @@ const items = ref([
 ]);
 
 const selected = ref({ value: 1, text: "Wade Cooper" });
+
+
+
+const datax = [
+  {
+    id: "#3949",
+    customer: "Albert Flores",
+    guests: 1,
+    checkIn: "Sep 12, 2 PM",
+    checkout: "Sep 14, 12 PM",
+    booked: "Aug 4, 2024, 12:44 PM",
+    totalPayout: "$115.50",
+    status: "Pending",
+    avatar: "https://via.placeholder.com/150"
+  },
+  {
+    id: "#3947",
+    customer: "Cameron Williamson",
+    guests: 2,
+    checkIn: "Sep 15, 4 PM",
+    checkout: "Sep 16, 12 PM",
+    booked: "Sep 2, 2024, 3:35 PM",
+    totalPayout: "$80.50",
+    status: "Upcoming",
+    avatar: "https://via.placeholder.com/150"
+  },
+  {
+    id: "#3946",
+    customer: "Annette Black",
+    guests: 2,
+    checkIn: "Sep 20, 2 PM",
+    checkout: "Sep 24, 12 PM",
+    booked: "Sep 19, 2024, 10:14 PM",
+    totalPayout: "$170.70",
+    status: "Pending",
+    avatar: "https://via.placeholder.com/150"
+  },
+  {
+    id: "#3943",
+    customer: "Darrell Steward",
+    guests: 1,
+    checkIn: "Sep 26, 2 PM",
+    checkout: "Sep 28, 12 PM",
+    booked: "Sep 10, 2024, 10:55 PM",
+    totalPayout: "$115.50",
+    status: "Active",
+    avatar: "https://via.placeholder.com/150"
+  },
+  {
+    id: "#3942",
+    customer: "Jerome Bell",
+    guests: 2,
+    checkIn: "Sep 28, 5 PM",
+    checkout: "Sep 30, 7 PM",
+    booked: "Sep 20, 2024, 1:45 PM",
+    totalPayout: "$130.40",
+    status: "Upcoming",
+    avatar: "https://via.placeholder.com/150"
+  }
+]
+
+const headers = ['#', 'Customer', 'Check-In', 'Checkout', 'Booked', 'Total Payout', 'Status', '']
+const widths = ['5%', '20%', '10%', '10%', '15%', '15%', '10%', '5%']
+
 </script>
 <template>
   <Layout>
@@ -67,7 +131,10 @@ const selected = ref({ value: 1, text: "Wade Cooper" });
             type="text"
           />
         </div>
+        <VTable :data="datax" :headers="headers" :widths="widths"></VTable>
       </template>
+     
     </VCard>
+   
   </Layout>
 </template>
